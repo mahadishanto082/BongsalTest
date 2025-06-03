dac7562503ad3993f4987c21d2c50b71dee1ff0f
mahadishanto082 <mahadishanto26@gmail.com>
2025-06-04 00:54:30 +0600

    first commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-commerce</title>
-    @vite(['resources/js/main.js']) <!-- ✅ Correct if all CSS is imported there -->
+    <title>Document</title>
+    @vite(['resources/js/app.js']) <!-- ✅ Correct if all CSS is imported there -->
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
@@ -16,7 +16,7 @@
 
     </app>
 
-    @vite('resources/js/main.js')
+    @vite('resources/js/app.js')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
   </body>
